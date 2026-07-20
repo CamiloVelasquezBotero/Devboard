e5c72c7c9ed2c43d84dec5e4f59c933db684c92c
Camilo <camilox168@outlook.com>
2026-07-20 14:36:26 -0500

    connection mongoose dns fixed

--- DevBoard_Server/src/config/db.ts
+++ DevBoard_Server/src/config/db.ts
@@ -1,5 +1,9 @@
 import mongoose from "mongoose";
 import colors from 'colors'
+import { setDefaultResultOrder } from 'node:dns'
+
+// Error to connect to mongoose with dns fixed 
+setDefaultResultOrder('ipv4first')
 
 export const connectDB = async () => {
     try {
@@ -8,7 +12,7 @@ export const connectDB = async () => {
         console.log(colors.cyan.bold(`MongoDB conectado en: ${url}`))
     } catch (error) {
         /* console.log(error.message) */
-        console.log(colors.red.bold("Error to connect to MongoDB"))
+        console.log(colors.red.bold("Error to connect to MongoDB:"), error)
         process.exit(1) /* o import { exit } from 'node:process'; */
     }
 }
