876ea30ce34b097bc76852b8c0478e2dbc2af45c
Camilo <camilox168@outlook.com>
2026-07-20 15:13:28 -0500

    bug fixed, connection from windows dns access

--- DevBoard_Server/src/config/db.ts
+++ DevBoard_Server/src/config/db.ts
@@ -1,13 +1,15 @@
 import mongoose from "mongoose";
 import colors from 'colors'
-import { setDefaultResultOrder } from 'node:dns'
+import { setDefaultResultOrder, setServers } from 'node:dns'
 
-// Error to connect to mongoose with dns fixed 
+// Bug Fixed, When MongoDB is connecting from Windows with the Node 17+ it doesn't work well with the IPv6. So, to solve it, 
+// we set the IPv4 first and use Google's DNS servers.
 setDefaultResultOrder('ipv4first')
+setServers(['8.8.8.8', '8.8.4.4', '1.1.1.1'])
 
 export const connectDB = async () => {
     try {
-        const connection = await mongoose.connect(process.env.DATABASE_URL)
+        const connection = await mongoose.connect(process.env.DATABASE_URL!)
         const url = `${connection.connection.host}:${connection.connection.port}`
         console.log(colors.cyan.bold(`MongoDB conectado en: ${url}`))
     } catch (error) {
